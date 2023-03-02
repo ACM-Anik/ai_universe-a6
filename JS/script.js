024a03c6fbec69b2d2a01b5626fb95ff9360bcf5
ACM-Anik <anikmojumder5252@gmail.com>
2023-03-02 23:27:32 +0600

    header added & fetched the data

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,14 @@
-console.log('everything okay');
+// console.log('everything okay');
+
+const fetchData = () => {
+    const url =`https://openapi.programming-hero.com/api/ai/tools`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayData(data.data.tools));
+}
+
+const displayData = (data) =>{
+    // console.log(data);
+    
+}
+fetchData();
